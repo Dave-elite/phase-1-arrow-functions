6db21892bee6d5ab7dd648c4686c3b098f8e5aac
David Waruhiu Njenga <davenjenga098@gmail.com>
2024-06-30 17:26:39 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide  = () => {
+     return 2000/100 
+}
+divide();
+
+const square = (parameter1) => {
+return parameter1 **parameter1
+}
+square();
+
+const add = (parameter1,parameter2) => {
+    return parameter1 + parameter2
+}
+add();
